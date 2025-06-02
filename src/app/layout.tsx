e3578ee4584988997af65db137fdd9f53dc74023
serge <korolariya2@gmail.com>
2025-06-02 11:09:34 +0300

    remove extra dep

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type {Metadata} from "next";
 import {Inter} from "next/font/google";
-import "./globals.css";
 import {DatabaseService} from "@/lib/DatabaseService";
 
 const inter = Inter({subsets: ["latin"]});

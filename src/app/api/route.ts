db95bc1a954a4fa2c1b96dbf0181bac3653213ea
serge <korolariya2@gmail.com>
2025-06-06 20:40:02 +0300

    DrizzleClient v3

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -3,6 +3,13 @@ import {createDrizzleClient} from "@/lib/DrizzleClient";
 import {users} from "@/lib/schema";
 
 export async function GET() {
+    console.log(process.env.SUPABASE_DB_HOST);
+    console.log(Number(process.env.SUPABASE_DB_PORT));
+    console.log(process.env.SUPABASE_DB_USER);
+    console.log(process.env.SUPABASE_DB_PASSWORD);
+    console.log(process.env.SUPABASE_DB_NAME);
+    console.log(process.env.SUPABASE_SSL_CERT_RAW);
+
     try {
         const db = createDrizzleClient();
         const allUsers = await db.select().from(users);
